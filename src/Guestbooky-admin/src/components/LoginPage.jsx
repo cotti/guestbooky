1bd452286b714c477dac5ca7109cbc137d60eda3
Felipe Cotti <felipe@cotti.com.br>
2024-11-18 14:00:07 -0300

    Add mobile navigation support.

--- src/Guestbooky-admin/src/components/LoginPage.jsx
+++ src/Guestbooky-admin/src/components/LoginPage.jsx
@@ -1,13 +1,12 @@
-import React, { useRef } from 'react';
+import React, {useRef} from 'react';
 import useAuth from '../hooks/useAuth.js';
 
 import './LoginPage.css'
 
-const LoginPage = ({onLoggedIn}) =>
-{
+const LoginPage = ({onLoggedIn}) => {
     const userInputRef = useRef(null);
     const passInputRef = useRef(null);
-    const { authenticate, error } = useAuth();
+    const {authenticate, error} = useAuth();
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -19,21 +18,19 @@ const LoginPage = ({onLoggedIn}) =>
                 if (error === null) {
                     onLoggedIn(true)
                 }
-        });
+            });
     }
 
-    return(
-        <>
+    return (
         <div className="login-form fade-in">
             <form action="post" onSubmit={handleSubmit}>
-                <label>User: </label><input type='text' name='username' placeholder=''
-                                                 ref={userInputRef}></input>
-                <label>Password: </label><input type='password' name='password' placeholder=''
+                <label htmlFor={'username'}>User: </label><input type='text' name='username' placeholder=''
+                                            ref={userInputRef}></input>
+                <label htmlFor={'password'}>Password: </label><input type='password' name='password' placeholder=''
                                                 ref={passInputRef}></input>
                 <button type='submit' className='login-button'>Login</button>
             </form>
         </div>
-        </>
     )
 }
 
